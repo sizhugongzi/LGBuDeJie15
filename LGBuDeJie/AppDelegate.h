af8e293a42b52879a1171612f8d64f33e46ddbab
sizhugongzi <228699456@qq.com>
2019-03-12 22:25:11 +0800

    Initial Commit

---
+++ LGBuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LGBuDeJie
+//
+//  Created by 甘延娇 on 2019/3/12.
+//  Copyright © 2019 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
